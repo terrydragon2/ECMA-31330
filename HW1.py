dbc082759a8d727aad756399c4b013368fe66bf9
terrydragon2 <57602248+terrydragon2@users.noreply.github.com>
2021-04-14 11:53:19 -0400

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -2,12 +2,7 @@ import numpy as np
 import random
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
-from seaborn import distplot 
-
-#I hope that's all the packages needed 
-#I deleted a few I thought I didn't use
-
-
+import statistics
 
 ################################
 ##Simulate DGP data 1000 times##
@@ -59,15 +54,16 @@ def DGP(N=1000, M=1000, alpha=0, beta_1=1, beta_2=0.1, rho=0.9):
         
         #Save number of X
         num_x.append(num)
-
+        
+    
     #print("The coefficients are:", coefX_1)
     #print("The confidence intervals are:", conf_int)
     #print("The number of X are:", num_x)
     #print("The X1 values are:", X_1)
     
     #Make plot of betas on X1 values
-    plt.hist([l[0] for l in coefX_1], bins=20) #chosen model
-    plt.hist([l[1] for l in coefX_1], alpha = 0.5, bins=20) #unrestricted model
+    plt.hist([l[0] for l in coefX_1], bins=25) #chosen model
+    plt.hist([l[1] for l in coefX_1], alpha = 0.5, bins=25) #unrestricted model
     
     #Get percentage of cases for which confidence interval captures beta
     count = 0
@@ -78,6 +74,31 @@ def DGP(N=1000, M=1000, alpha=0, beta_1=1, beta_2=0.1, rho=0.9):
     print("The percentage is:", percent)
     
     
+    unr_var = statistics.variance(l[1] for l in coefX_1)
+    chosen_var = statistics.variance(l[0] for l in coefX_1)
+    print("The variance of chosen betas is", chosen_var)
+    print("The variance of unrestricted betas is", unr_var)
+    
+DGP(N=50)
 
-#Test it out
-DGP(N=1000, M=1000)
+DGP(N=100)
+
+DGP(N=200)
+
+DGP(N=1000)
+
+DGP(rho=0.1)
+
+DGP(rho=0.5)
+
+DGP(rho=0.9)
+
+DGP(beta_2=0.01)
+
+DGP(beta_2=0.05)
+
+DGP(beta_2=0.1)
+
+DGP(beta_2=0.2)
+
+DGP(beta_2=1)
